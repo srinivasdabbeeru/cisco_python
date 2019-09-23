adb83a3089c6abdfd0d003934319134c9027e960
srinivasdabbeeru <srinivas.daberru@gmail.com>
2019-09-23 14:49:57 +0530

    done

---
+++ if.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from time import ctime,sleep
+from subprocess import getstatusoutput,getoutput
+
+#only importing desired dunctions
+
+options='''
+press 1 to check current date and time:
+press 2 to run any command in your current os:
+press 3 to create a directory:
+press 4 to start a train:
+press 5 to ping any website:
+'''
+print(options)
+# to capture input from user
+choice=input()
+print ("you have chosen ", choice)
+
